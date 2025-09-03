e68cf7b4639cf3115c2f4732231fc57d62b3ac6e
Nikita <NikDok05@gmail.com>
2025-09-03 17:08:11 +0300

    (develop) Добавлены считывание переменной, функция f()

--- Dokudin_0.cpp
+++ Dokudin_0.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
 
+void f() {
+    int x;
+}
+
 int main() {
+    int x;
+    std::cin >> x;
+    f();
     std::cout << "Hello world" << std::endl;
     std::getchar();
     return 0;
